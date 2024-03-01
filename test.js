fba51c507dc756c3c119c505d4183b03ccf1d304
roshanpatil <rproshan07@gmail.com>
2024-03-01 16:03:40 +0530

    data-type

--- test.js
+++ test.js
@@ -1,2 +1,29 @@
 console.log("hello");
 
+let score = "33"
+
+// console.log(typeof score);
+
+let name ="roshan"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not declare
+// symbol => unique 
+
+// object ----
+
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
