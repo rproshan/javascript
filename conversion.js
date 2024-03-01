c24a3568fd396394bae158bf7fdcfd79c4a3436d
roshanpatil <rproshan07@gmail.com>
2024-03-01 16:21:25 +0530

    conversion

---
+++ conversion.js
@@ -0,0 +1,34 @@
+let score = "roshan"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "22" => 22
+
+// "33abc" => Nan
+
+// true => 1 ; false => 0
+
+let isLoggedIn = "roshan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+
+// "" => false
+
+// "roshan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
---
+++ dataType.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+// console.log(typeof score);
+
+let name ="roshan"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not declare
+// symbol => unique 
+
+// object ----
+
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
+
+
