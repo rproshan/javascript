3683b44a2da74e0627e5b8e8d2bf688f0b95c213
roshanpatil <rproshan07@gmail.com>
2024-03-05 17:58:15 +0530

    comparison

--- conversion.js
+++ conversion.js
@@ -30,5 +30,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " roshan"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gamecounter = 100
+gamecounter++;
+
+console.log(gamecounter);
+gamecounter++;
+
+console.log(gamecounter);
+
+
+
