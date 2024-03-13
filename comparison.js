a3edb8d499c0f3b3e6fa32529e3a97c5a78785a8
roshanpatil <rproshan07@gmail.com>
2024-03-13 13:01:55 +0530

    data-type

--- comparison.js
+++ comparison.js
@@ -10,7 +10,6 @@ console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-
 console.log("2" === 2);
 
 
---
+++ datatype-summery.js
@@ -0,0 +1,35 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100;
+// const scoreValue = 100.20;
+
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let userEmail;
+
+// const id = Symbol('123');
+// const anotherid = Symbol('123');
+
+// console.log(id === anotherid);
+
+
+const bigNumber = 342634562567654654n;
+
+console.log(bigNumber);
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["A", "B", "C" ]
+
+let myObj = {
+    name: "roshan",
+    age: 30,
+}
+
+console.log(myObj);
+
